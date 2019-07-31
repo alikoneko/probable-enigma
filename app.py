21ed587c152617e2e340abda3fbf50d1758358c3
ali neer <ali@alineer.com>
2019-07-31 10:38:17 -0400

    forms work

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 @app.route('/defang/', methods=["GET", "POST"])
 def defang(url="https://twitter.com/comradeeevee"):
+    url = request.form['defang']
     defanged = url.replace("https", "hxxps").replace(".", "[.]").replace("/", "/\\")
     return f'defanged: {defanged}' 
 
 @app.route('/refang/', methods=["GET", "POST"])
-def refang(url="hxxps:/\/\\twitter[.]com/\comradeeevee"):
+def refang(url="hxxps:/\/\\twitter.com/\comradeeevee"):
+    url = request.form['refang']
     refanged = url.replace("hxxps", "https").replace("[.]", ".").replace("/\\", "/")
     return f'refanged: {refanged}'
 
